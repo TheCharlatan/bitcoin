343d1afb4a40e04d032098777fe9e2f39142a14c
TheCharlatan <seb.kung@gmail.com>
2023-02-18 12:57:39 +0100

    refactor, BlockManager: Replace gArgs with options
    Remove access to the global gArgs from within the BlockManager class and
    replace it with a dedicated struct holding this value for the lifetime
    of the BlockManager.
    
    This should eventually allow users of the BlockManager to not rely on
    the global gArgs and instead pass in their own options.

---
+++ src/node/blockmanager_args.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2023 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <node/blockmanager_args.h>
+#include <util/system.h>
+
+namespace node {
+void ApplyArgsManOptions(const ArgsManager& args, BlockManager::Options& opts)
+{
+    if (auto value{args.GetBoolArg("-fastprune")}) opts.fast_prune = *value;
+}
+} // namespace node
