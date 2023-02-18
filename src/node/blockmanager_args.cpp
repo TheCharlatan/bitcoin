39fa0de08adc2cd19081bb5679d26a6b8b6cb331
TheCharlatan <seb.kung@gmail.com>
2023-02-18 12:57:39 +0100

    refactor, BlockManager: Replace gArgs with options
    Remove access to the global gArgs from within the BlockManager class and
    replace it with a dedicated struct holding this value for the lifetime
    of the BlockManager.
    
    This should eventually allow users of the BlockManager to not rely on
    the global gArgs and instead pass in their own options.

--- src/node/blockmanager_args.cpp
+++ src/node/blockmanager_args.cpp
@@ -7,6 +7,8 @@
 #include <util/system.h>
 #include <validation.h>
 
+#include <optional>
+
 namespace node {
 std::optional<bilingual_str> ApplyArgsManOptions(const ArgsManager& args, BlockManager::Options& opts)
 {
@@ -24,6 +26,8 @@ std::optional<bilingual_str> ApplyArgsManOptions(const ArgsManager& args, BlockM
         }
     }
     opts.prune_target = nPruneTarget;
+
+    if (auto value{args.GetBoolArg("-fastprune")}) opts.fast_prune = *value;
 
     return std::nullopt;
 }
