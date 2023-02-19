3ded321b878e7ad2bdbd9d542ffb2e36428396ec
TheCharlatan <seb.kung@gmail.com>
2023-02-19 10:20:34 +0100

    refactor, blockstorage: Replace stopafterblockimport arg
    Add a stop_after_block_import field to the BlockManager options. Use
    this field instead of the global gArgs.
    
    This should allow users of the BlockManager to not rely on the global
    Args.

--- src/kernel/blockmanager_opts.h
+++ src/kernel/blockmanager_opts.h
@@ -9,12 +9,15 @@
 
 namespace kernel {
 
+static constexpr bool DEFAULT_STOPAFTERBLOCKIMPORT{false};
+
 /**
  * An options struct for `BlockManager`, more ergonomically referred to as
  * `BlockManager::Options` due to the using-declaration in `BlockManager`
  */
 struct BlockManagerOpts {
     bool fast_prune{false};
+    bool stop_after_block_import{DEFAULT_STOPAFTERBLOCKIMPORT};
     fs::path blocks_dir;
 };
 } // namespace kernel
