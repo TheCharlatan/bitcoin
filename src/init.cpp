add8eaf6beebc84dd9383055c95dfe30f4817e23
TheCharlatan <seb.kung@gmail.com>
2023-05-03 22:24:21 +0200

    zmq: Pass lambda to zmq's ZMQPublishRawBlockNotifier
    The lambda captures the previously introduced getBlockByIndex from the
    chain interface.
    
    This is done in preparation for the following commits where blockstorage
    functions are made BlockManager methods.

--- src/init.cpp
+++ src/init.cpp
@@ -1420,7 +1420,10 @@ bool AppInitMain(NodeContext& node, interfaces::BlockAndHeaderTipInfo* tip_info)
     }
 
 #if ENABLE_ZMQ
-    g_zmq_notification_interface = CZMQNotificationInterface::Create();
+    g_zmq_notification_interface = CZMQNotificationInterface::Create(
+        [&chain = node.chain](CBlock& block, const CBlockIndex* index) {
+            return chain->getBlockByIndex(block, index);
+        });
 
     if (g_zmq_notification_interface) {
         RegisterValidationInterface(g_zmq_notification_interface.get());
