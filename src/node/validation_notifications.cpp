620faa21ce0210dddca243511b7674c3b8f4e901
TheCharlatan <seb.kung@gmail.com>
2023-05-10 22:29:17 +0200

    refactor: Pass BlockNotify in options to ChainstateManager
    This commit is part of the libbitcoinkernel project and seeks to remove
    the ChainstateManager's and, more generally, the kernel library's
    dependency on interface_ui with options methods in this and the following
    few commits. By removing interface_ui from the kernel library, its
    dependency on boost is reduced to just boost::multi_index.
    
    Create a new file in the node library for defining a function creating
    the default set of notification methods such that these do not need to
    be re-defined all over the codebase.

---
+++ src/node/validation_notifications.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2023 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <node/validation_notifications.h>
+
+#include <node/interface_ui.h>
+
+namespace node {
+
+void ValidationNotificationsImpl::notifyBlockTip(SynchronizationState state, CBlockIndex* index)
+{
+    uiInterface.NotifyBlockTip(state, index);
+}
+
+} // namespace node
