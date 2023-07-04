fdeecb613c97e8e004070810c6af9c74e187a06c
TheCharlatan <seb.kung@gmail.com>
2023-07-04 22:31:19 +0200

    Point depends at my own native_bitcoin_tidy fork

--- depends/packages/native_bitcoin_tidy.mk
+++ depends/packages/native_bitcoin_tidy.mk
@@ -1,8 +1,8 @@
 package=native_bitcoin_tidy
-$(package)_version=89efb2d5ba33ae201f00c89cba1abf1ee47b443b
-$(package)_download_path=https://github.com/theuni/bitcoin-tidy-experiments/archive
+$(package)_version=12cd64674cb477136ea9bce9110bf98f4c688061
+$(package)_download_path=https://github.com/TheCharlatan/bitcoin-tidy-experiments/archive
 $(package)_file_name=$($(package)_version).tar.gz
-$(package)_sha256_hash=9cb3de45adf3ec60344d02c3d8c69567efc098a9c0a082de2e758307fbf86ff2
+$(package)_sha256_hash=50568033ff1b1e6d7af8c899bd82e5d3dbba9cd49aab0b4dff124e1941a9385f
 $(package)_dependencies=native_clang_headers
 define $(package)_config_cmds
   $($(package)_cmake)
