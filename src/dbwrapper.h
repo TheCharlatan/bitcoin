4735590b9f3bdfc248086e0e20c9d56fa6156e1c
TheCharlatan <seb.kung@gmail.com>
2023-07-13 21:17:45 +0200

    refactor: Wrap DestroyDB in dbwrapper helper
    Wrap leveldb::DestroyDB in a helper function without exposing
    leveldb-specifics.
    
    Also, add missing optional include.
    
    The context of this commit is an effort to decouple the dbwrapper header
    file from leveldb includes. To this end, the includes are moved to the
    dbwrapper implementation file. This is done as part of the kernel
    project to reduce the number of required includes for users of the
    kernel.

--- src/dbwrapper.h
+++ src/dbwrapper.h
@@ -21,6 +21,7 @@
 #include <leveldb/slice.h>
 #include <leveldb/status.h>
 #include <leveldb/write_batch.h>
+#include <optional>
 #include <stdexcept>
 #include <string>
 #include <vector>
@@ -64,10 +65,6 @@ public:
 
 class CDBWrapper;
 
-namespace dbwrapper {
-    using leveldb::DestroyDB;
-}
-
 /** These should be considered an implementation detail of the specific database.
  */
 namespace dbwrapper_private {
@@ -83,6 +80,8 @@ void HandleError(const leveldb::Status& status);
 const std::vector<unsigned char>& GetObfuscateKey(const CDBWrapper &w);
 
 };
+
+bool DestroyDB(std::string& path_str);
 
 /** Batch of changes queued to be written to a CDBWrapper */
 class CDBBatch
