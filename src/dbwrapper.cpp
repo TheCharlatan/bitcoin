3523f4f1c4c488be397883d19ea961670e2d1bdb
TheCharlatan <seb.kung@gmail.com>
2023-07-14 11:53:24 +0200

    refactor: Split dbwrapper CDBWrapper::Exists implementation
    Keep the generic serialization in the header, while moving
    leveldb-specifics to the implementation file.
    
    The context of this commit is an effort to decouple the dbwrapper header
    file from leveldb includes. To this end, the includes are moved to the
    dbwrapper implementation file. This is done as part of the kernel
    project to reduce the number of required includes for users of the
    kernel.

--- src/dbwrapper.cpp
+++ src/dbwrapper.cpp
@@ -322,6 +322,21 @@ bool CDBWrapper::ReadImpl(std::function<void(DataStream&)> write_key_to_stream, 
     return true;
 }
 
+bool CDBWrapper::ExistsImpl(const DataStream& ssKey) const
+{
+    leveldb::Slice slKey(CharCast(ssKey.data()), ssKey.size());
+
+    std::string strValue;
+    leveldb::Status status = pdb->Get(readoptions, slKey, &strValue);
+    if (!status.ok()) {
+        if (status.IsNotFound())
+            return false;
+        LogPrintf("LevelDB read failure: %s\n", status.ToString());
+        dbwrapper_private::HandleError(status);
+    }
+    return true;
+}
+
 bool CDBWrapper::IsEmpty()
 {
     std::unique_ptr<CDBIterator> it(NewIterator());
