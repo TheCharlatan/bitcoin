029ac57bf6a987f50c31bb8c80e77989fc53d85a
TheCharlatan <seb.kung@gmail.com>
2023-07-29 12:03:35 +0200

    refactor: Pimpl leveldb::Iterator for CDBIterator
    Hide the leveldb::Iterator member variable with a pimpl in order not to
    expose it directly in the header.
    
    Also, move CDBWrapper::NewIterator to the dbwrapper implementation to
    use the pimpl for CDBIterator initialziation.
    
    The context of this commit is an effort to decouple the dbwrapper header
    file from leveldb includes. To this end, the includes are moved to the
    dbwrapper implementation file. This is done as part of the kernel
    project to reduce the number of required includes for users of the
    kernel.

--- src/dbwrapper.h
+++ src/dbwrapper.h
@@ -26,7 +26,6 @@
 #include <vector>
 namespace leveldb {
 class Env;
-class Iterator;
 }
 
 static const size_t DBWRAPPER_PREALLOC_KEY_SIZE = 64;
@@ -133,9 +132,12 @@ public:
 
 class CDBIterator
 {
+public:
+    struct IteratorImpl;
+
 private:
     const CDBWrapper &parent;
-    leveldb::Iterator *piter;
+    const std::unique_ptr<IteratorImpl> m_pimpl_iter;
 
     void SeekImpl(DataStream& ssKey);
     Span<const std::byte> GetKeyImpl() const;
@@ -147,8 +149,7 @@ public:
      * @param[in] _parent          Parent CDBWrapper instance.
      * @param[in] _piter           The original leveldb iterator.
      */
-    CDBIterator(const CDBWrapper &_parent, leveldb::Iterator *_piter) :
-        parent(_parent), piter(_piter) { };
+    CDBIterator(const CDBWrapper& _parent, std::unique_ptr<IteratorImpl> _piter);
     ~CDBIterator();
 
     bool Valid() const;
@@ -312,10 +313,7 @@ public:
     // Get an estimate of LevelDB memory usage (in bytes).
     size_t DynamicMemoryUsage() const;
 
-    CDBIterator *NewIterator()
-    {
-        return new CDBIterator(*this, pdb->NewIterator(iteroptions));
-    }
+    CDBIterator *NewIterator();
 
     /**
      * Return true if the database managed by this class contains no entries.
