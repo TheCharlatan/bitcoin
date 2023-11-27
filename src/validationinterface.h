35ee50f762aee78944f563b74a5e9a677980400d
TheCharlatan <seb.kung@gmail.com>
2023-11-27 11:46:36 +0100

    [refactor] Make MainSignalsImpl RAII styled

--- src/validationinterface.h
+++ src/validationinterface.h
@@ -161,13 +161,10 @@ private:
     std::unique_ptr<ValidationSignalsImpl> m_internals;
 
 public:
-    ValidationSignals();
+    ValidationSignals(CScheduler& scheduler LIFETIMEBOUND);
+
     ~ValidationSignals();
 
-    /** Register a CScheduler to give callbacks which should run in the background (may only be called once) */
-    void RegisterBackgroundSignalScheduler(CScheduler& scheduler);
-    /** Unregister a CScheduler to give callbacks which should run in the background - these callbacks will now be dropped! */
-    void UnregisterBackgroundSignalScheduler();
     /** Call any remaining callbacks on the calling thread */
     void FlushBackgroundCallbacks();
 
