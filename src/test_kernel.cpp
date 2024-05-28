e38e92db96ba309407baec8014b2eed49f2c16c3
TheCharlatan <seb.kung@gmail.com>
2024-05-28 10:32:52 +0200

    Kernel C API
    Implement libbitcoinconsensus equivalent

---
+++ src/test_kernel.cpp
@@ -0,0 +1,96 @@
+// Copyright (c) 2024-present The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <kernel/bitcoinkernel.h>
+
+#include <algorithm>
+#include <cassert>
+#include <cstdint>
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+
+struct ByteArray {
+    const unsigned char* data;
+    size_t size;
+};
+
+
+ByteArray hex_string_to_byte_array(const std::string& hex)
+{
+    std::vector<unsigned char> bytes;
+
+    for (size_t i{0}; i < hex.length(); i += 2) {
+        std::string byteString{hex.substr(i, 2)};
+        unsigned char byte = (char)std::strtol(byteString.c_str(), nullptr, 16);
+        bytes.push_back(byte);
+    }
+
+    std::unique_ptr<unsigned char[]> byte_array(new unsigned char[bytes.size()]);
+    std::copy(bytes.begin(), bytes.end(), byte_array.get());
+
+    ByteArray result;
+    result.data = byte_array.release();
+    result.size = bytes.size();
+    return result;
+}
+
+const auto VERIFY_ALL_PRE_TAPROOT = kernel_SCRIPT_FLAGS_VERIFY_P2SH | kernel_SCRIPT_FLAGS_VERIFY_DERSIG |
+                                    kernel_SCRIPT_FLAGS_VERIFY_NULLDUMMY | kernel_SCRIPT_FLAGS_VERIFY_CHECKLOCKTIMEVERIFY |
+                                    kernel_SCRIPT_FLAGS_VERIFY_CHECKSEQUENCEVERIFY | kernel_SCRIPT_FLAGS_VERIFY_WITNESS;
+
+const auto VERIFY_ALL_PRE_SEGWIT = kernel_SCRIPT_FLAGS_VERIFY_P2SH | kernel_SCRIPT_FLAGS_VERIFY_DERSIG |
+                                   kernel_SCRIPT_FLAGS_VERIFY_NULLDUMMY | kernel_SCRIPT_FLAGS_VERIFY_CHECKLOCKTIMEVERIFY |
+                                   kernel_SCRIPT_FLAGS_VERIFY_CHECKSEQUENCEVERIFY;
+
+
+void assert_error_ok(kernel_Error& error)
+{
+    assert(error.code == kernel_ErrorCode::kernel_ERROR_OK);
+}
+
+void verify_test(std::string spent, std::string spending, int64_t amount, unsigned int nIn)
+{
+    ByteArray script_pubkey{hex_string_to_byte_array(spent)};
+    ByteArray spending_tx{hex_string_to_byte_array(spending)};
+    kernel_Error error{};
+    error.code = kernel_ErrorCode::kernel_ERROR_OK;
+
+    assert(kernel_verify_script_with_amount(
+        script_pubkey.data,
+        script_pubkey.size,
+        amount,
+        spending_tx.data,
+        spending_tx.size,
+        nIn,
+        VERIFY_ALL_PRE_TAPROOT,
+        &error));
+
+    assert(kernel_verify_script(
+        script_pubkey.data,
+        script_pubkey.size,
+        spending_tx.data,
+        spending_tx.size,
+        nIn,
+        VERIFY_ALL_PRE_SEGWIT,
+        &error));
+
+    delete[] script_pubkey.data;
+    delete[] spending_tx.data;
+
+    assert_error_ok(error);
+}
+
+int main()
+{
+    verify_test(
+        "76a9144bfbaf6afb76cc5771bc6404810d1cc041a6933988ac",
+        "02000000013f7cebd65c27431a90bba7f796914fe8cc2ddfc3f2cbd6f7e5f2fc854534da95000000006b483045022100de1ac3bcdfb0332207c4a91f3832bd2c2915840165f876ab47c5f8996b971c3602201c6c053d750fadde599e6f5c4e1963df0f01fc0d97815e8157e3d59fe09ca30d012103699b464d1d8bc9e47d4fb1cdaa89a1c5783d68363c4dbc4b524ed3d857148617feffffff02836d3c01000000001976a914fc25d6d5c94003bf5b0c7b640a248e2c637fcfb088ac7ada8202000000001976a914fbed3d9b11183209a57999d54d59f67c019e756c88ac6acb0700",
+        0, 0);
+
+    std::cout << "Libbitcoinkernel test completed.\n";
+    return 0;
+}
