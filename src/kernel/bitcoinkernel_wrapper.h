08a52f02bf8472499604b7aae4a88e674d6182a8
TheCharlatan <seb.kung@gmail.com>
2024-05-30 11:22:28 +0200

    kernel: Add import blocks function to C header
    The `kernel_import_blocks` function is used to both trigger a reindex,
    if the indexes were previously wiped through the chainstate load
    options, or import the block data of a single block file.
    
    The behaviour of the import can be verified through the test logs.

--- src/kernel/bitcoinkernel_wrapper.h
+++ src/kernel/bitcoinkernel_wrapper.h
@@ -400,9 +400,23 @@ public:
     ChainMan(const ChainMan&) = delete;
     ChainMan& operator=(const ChainMan&) = delete;
 
-    bool LoadChainstate(const ChainstateLoadOptions& chainstate_load_opts) const noexcept
+    bool LoadChainstate(ChainstateLoadOptions& chainstate_load_opts) const noexcept
     {
         return kernel_chainstate_manager_load_chainstate(m_context.m_context.get(), chainstate_load_opts.m_options.get(), m_chainman);
+    }
+
+    bool ImportBlocks(const std::span<const std::string> paths) const noexcept
+    {
+        std::vector<const char*> c_paths;
+        std::vector<size_t> c_paths_lens;
+        c_paths.reserve(paths.size());
+        c_paths_lens.reserve(paths.size());
+        for (const auto& path : paths) {
+            c_paths.push_back(path.c_str());
+            c_paths_lens.push_back(path.length());
+        }
+
+        return kernel_import_blocks(m_context.m_context.get(), m_chainman, c_paths.data(), c_paths_lens.data(), c_paths.size());
     }
 
     bool ProcessBlock(const Block& block, bool* new_block) const noexcept
