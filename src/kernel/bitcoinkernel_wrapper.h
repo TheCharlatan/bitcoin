385f05d25551432a6921707bde913c7bab7c1142
TheCharlatan <seb.kung@gmail.com>
2024-06-01 11:52:52 +0200

    kernel: Add function for copying block data to C header
    This adds functions for copying serialized block data into a user-owned
    variable-sized byte array.
    
    Use it in the tests for verifying the implementation of the validation
    interface's `BlockChecked` method.

--- src/kernel/bitcoinkernel_wrapper.h
+++ src/kernel/bitcoinkernel_wrapper.h
@@ -567,6 +567,11 @@ public:
     UnownedBlock& operator=(const UnownedBlock&) = delete;
     UnownedBlock(UnownedBlock&&) = delete;
     UnownedBlock& operator=(UnownedBlock&&) = delete;
+
+    std::vector<std::byte> ToBytes() const
+    {
+        return write_bytes(m_block, btck_block_pointer_to_bytes);
+    }
 };
 
 class BlockValidationState
@@ -753,6 +758,11 @@ public:
     auto Transactions() const
     {
         return Range<Block, &Block::CountTransactions, &Block::GetTransaction>{*this};
+    }
+
+    std::vector<std::byte> ToBytes() const
+    {
+        return write_bytes(impl(), btck_block_to_bytes);
     }
 
     friend class ChainMan;
