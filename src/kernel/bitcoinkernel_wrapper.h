82d663ba856fb6708f9a9d3c39d6265bdf5517ff
TheCharlatan <seb.kung@gmail.com>
2024-06-03 14:44:55 +0200

    kernel: Add chain params context option to C header
    As a first option, add the chainparams. For now these can only be
    instantiated with default values. In future they may be expanded to take
    their own options for regtest and signet configurations.
    
    This commit also introduces a unique pattern for setting the option
    values when calling the `*_set(...)` function.

--- src/kernel/bitcoinkernel_wrapper.h
+++ src/kernel/bitcoinkernel_wrapper.h
@@ -229,6 +229,24 @@ public:
     }
 };
 
+class ChainParams
+{
+private:
+    struct Deleter {
+        void operator()(btck_ChainParameters* ptr) const noexcept
+        {
+            btck_chain_parameters_destroy(ptr);
+        }
+    };
+
+    std::unique_ptr<btck_ChainParameters, Deleter> m_chain_params;
+
+public:
+    ChainParams(btck_ChainType chain_type) : m_chain_params{check(btck_chain_parameters_create(chain_type))} {}
+
+    friend class ContextOptions;
+};
+
 class ContextOptions
 {
 private:
@@ -243,6 +261,11 @@ private:
 
 public:
     ContextOptions() : m_options{check(btck_context_options_create())} {}
+
+    void SetChainParams(ChainParams& chain_params) const
+    {
+        btck_context_options_set_chainparams(m_options.get(), chain_params.m_chain_params.get());
+    }
 
     friend class Context;
 };
