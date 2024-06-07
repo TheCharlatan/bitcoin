682f1f1827dff78208bc1272f82d217c42a2cd69
Ryan Ofsky <ryan@ofsky.org>
2024-06-07 13:05:06 +0200

    blockman: Replace m_reindexing with m_blockfiles_indexed
    This is a just a mechanical change, renaming and inverting the meaning
    of the indexing variable.
    
    "m_blockfiles_indexed" is a more straightforward name for this variable
    because this variable just indicates whether or not
    <datadir>/blocks/blk?????.dat files have been indexed in the
    <datadir>/blocks/index LevelDB database. The name "m_reindexing" was
    more confusing, it could be true even if -reindex was not specified, and
    false when it was specified. Also, the previous name unnecessarily
    required thinking about the whole reindexing process just to understand
    simple checks in validation code about whether blocks were indexed.
    
    The motivation for this change is to follow up on previous commits,
    moving away from having multiple variables called "reindex" internally,
    and instead naming variables individually after what they do and
    represent.

--- src/init.cpp
+++ src/init.cpp
@@ -1670,7 +1670,7 @@ bool AppInitMain(NodeContext& node, interfaces::BlockAndHeaderTipInfo* tip_info)
     // if pruning, perform the initial blockstore prune
     // after any wallet rescanning has taken place.
     if (chainman.m_blockman.IsPruneMode()) {
-        if (!chainman.m_blockman.m_reindexing) {
+        if (chainman.m_blockman.m_blockfiles_indexed) {
             LOCK(cs_main);
             for (Chainstate* chainstate : chainman.GetAll()) {
                 uiInterface.InitMessage(_("Pruning blockstore…").translated);
@@ -1696,7 +1696,7 @@ bool AppInitMain(NodeContext& node, interfaces::BlockAndHeaderTipInfo* tip_info)
     int chain_active_height = WITH_LOCK(cs_main, return chainman.ActiveChain().Height());
 
     // On first startup, warn on low block storage space
-    if (!chainman.m_blockman.m_reindexing && !do_reindex_chainstate && chain_active_height <= 1) {
+    if (!chainman.m_blockman.m_blockfiles_indexed && !do_reindex_chainstate && chain_active_height <= 1) {
         uint64_t assumed_chain_bytes{chainparams.AssumedBlockchainSize() * 1024 * 1024 * 1024};
         uint64_t additional_bytes_needed{
             chainman.m_blockman.IsPruneMode() ?
