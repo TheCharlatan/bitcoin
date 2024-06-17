ad0875e3976e6f17b55a20c982f1b2c3d768eabd
TheCharlatan <seb.kung@gmail.com>
2024-06-17 22:50:23 +0200

    kernel: Add block validation to C header
    The added function allows the user process and validate a given block
    with the chainstate manager. The *_process_block(...) function does some
    preliminary checks on the block before passing it to
    `ProcessNewBlock(...)`. These are similar to the checks in the
    `submitblock()` rpc.
    
    Richer processing of the block validation result will be made available
    in the following commits through the validation interface.
    
    The commits also adds a utility for serializing a `CBlock`
    (`kernel_block_create()`) that may then be passed to the library for
    processing.
    
    The tests exercise the function for both mainnet and regtest. The
    commit also adds the data of 206 regtest blocks (some blocks also
    contain transactions).

--- src/kernel/bitcoinkernel.h
+++ src/kernel/bitcoinkernel.h
@@ -192,6 +192,11 @@ typedef struct kernel_ChainstateManager kernel_ChainstateManager;
  */
 typedef struct kernel_ChainstateLoadOptions kernel_ChainstateLoadOptions;
 
+/**
+ * Opaque data structure for holding a block.
+ */
+typedef struct kernel_Block kernel_Block;
+
 /** Current sync state passed to tip changed callbacks. */
 typedef enum {
     kernel_INIT_REINDEX,
@@ -711,9 +716,48 @@ bool BITCOINKERNEL_WARN_UNUSED_RESULT kernel_chainstate_manager_load_chainstate(
 ) BITCOINKERNEL_ARG_NONNULL(1, 2, 3);
 
 /**
+ * @brief Process and validate the passed in block with the chainstate manager.
+ *
+ * @param[in] context            Non-null.
+ * @param[in] chainstate_manager Non-null.
+ * @param[in] block              Non-null, block to be validated.
+ * @param[out] new_block         Nullable, will be set to true if this block was not processed before, and false otherwise.
+ * @return                       True if processing the block was successful. Will also return true for valid, but duplicate blocks.
+ */
+bool BITCOINKERNEL_WARN_UNUSED_RESULT kernel_chainstate_manager_process_block(
+    const kernel_Context* context,
+    kernel_ChainstateManager* chainstate_manager,
+    kernel_Block* block,
+    bool* new_block
+) BITCOINKERNEL_ARG_NONNULL(1, 2, 3);
+
+/**
  * Destroy the chainstate manager.
  */
 void kernel_chainstate_manager_destroy(kernel_ChainstateManager* chainstate_manager, const kernel_Context* context);
+
+///@}
+
+/** @name Block
+ * Functions for working with blocks.
+ */
+///@{
+
+/**
+ * @brief Parse a serialized raw block into a new block object.
+ *
+ * @param[in] raw_block     Non-null, serialized block.
+ * @param[in] raw_block_len Length of the serialized block.
+ * @return                  The allocated block, or null on error.
+ */
+kernel_Block* BITCOINKERNEL_WARN_UNUSED_RESULT kernel_block_create(
+    const unsigned char* raw_block, size_t raw_block_len
+) BITCOINKERNEL_ARG_NONNULL(1);
+
+/**
+ * Destroy the block.
+ */
+void kernel_block_destroy(kernel_Block* block);
 
 ///@}
 
