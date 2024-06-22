6d352127f83b2822faf064adebaff83a445bd511
TheCharlatan <seb.kung@gmail.com>
2024-06-22 21:08:10 +0200

    kernel: Add chainstate load options for in-memory dbs in C header
    This allows a user to run the kernel without creating on-disk files for
    the block tree and chainstate indexes. This is potentially useful in
    scenarios where the user needs to do some ephemeral validation
    operations.
    
    One specific use case is when linearizing the blocks on disk. The block
    files store blocks out of order, so a program may utilize the library
    and its header to read the blocks with one chainstate manager, and then
    write them back in order, and without orphans, with another chainstate
    maanger. To save disk resources and if the indexes are not required once
    done, it may be beneficial to keep the indexes in memory for the
    chainstate manager that writes the blocks back again.

--- src/kernel/bitcoinkernel.h
+++ src/kernel/bitcoinkernel.h
@@ -706,6 +706,28 @@ BITCOINKERNEL_API void kernel_chainstate_load_options_set_wipe_chainstate_db(
 ) BITCOINKERNEL_ARG_NONNULL(1);
 
 /**
+ * @brief Sets block tree db in memory in the chainstate load options.
+ *
+ * @param[in] chainstate_load_options Non-null, created by @ref kernel_chainstate_load_options_create.
+ * @param[in] block_tree_db_in_memory Set block tree db in memory.
+ */
+BITCOINKERNEL_API void kernel_chainstate_load_options_set_block_tree_db_in_memory(
+    kernel_ChainstateLoadOptions* chainstate_load_options,
+    bool block_tree_db_in_memory
+) BITCOINKERNEL_ARG_NONNULL(1);
+
+/**
+ * @brief Sets chainstate db in memory in the chainstate load options.
+ *
+ * @param[in] chainstate_load_options Non-null, created by @ref kernel_chainstate_load_options_create.
+ * @param[in] chainstate_db_in_memory Set chainstate db in memory.
+ */
+BITCOINKERNEL_API void kernel_chainstate_load_options_set_chainstate_db_in_memory(
+    kernel_ChainstateLoadOptions* chainstate_load_options,
+    bool chainstate_db_in_memory
+) BITCOINKERNEL_ARG_NONNULL(1);
+
+/**
  * Destroy the chainstate load options
  */
 BITCOINKERNEL_API void kernel_chainstate_load_options_destroy(kernel_ChainstateLoadOptions* chainstate_load_options);
