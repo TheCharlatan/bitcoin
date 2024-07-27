50c14e5a170950f30aa6b474bb231bbbfdd82427
TheCharlatan <seb.kung@gmail.com>
2024-07-27 16:36:02 +0200

    kernel: Only setup kernel context globals once
    The globals setup by the function calls when creating a new kernel
    context only need to be setup once. Calling them multiple times may be
    wasteful and has no apparent benefit.
    
    Besides kernel users potentially creating multiple contexts, this change
    may also be useful for tests creating multiple setups.

--- src/kernel/context.cpp
+++ src/kernel/context.cpp
@@ -10,13 +10,16 @@
 
 #include <string>
 
-
 namespace kernel {
 Context::Context()
 {
-    std::string sha256_algo = SHA256AutoDetect();
-    LogPrintf("Using the '%s' SHA256 implementation\n", sha256_algo);
-    RandomInit();
+    static bool initialized{false};
+    if (!initialized) {
+        std::string sha256_algo = SHA256AutoDetect();
+        LogInfo("Using the '%s' SHA256 implementation\n", sha256_algo);
+        RandomInit();
+    }
+    initialized = true;
 }
 
 
