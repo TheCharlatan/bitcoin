e72cf7122e41aae8dc2434f2ef70b6e300a186c6
TheCharlatan <seb.kung@gmail.com>
2024-08-29 15:04:30 +0200

    kernel: Add check for when a block has been mutated.

--- src/kernel/bitcoinkernel.cpp
+++ src/kernel/bitcoinkernel.cpp
@@ -1369,3 +1369,8 @@ kernel_ByteArray* kernel_copy_block_header_data(kernel_BlockHeader* header_)
     return byte_array;
 }
 
+bool kernel_is_block_mutated(kernel_Block* block_, bool check_witness_root)
+{
+    auto block{cast_cblocksharedpointer(block_)};
+    return IsBlockMutated(**block, check_witness_root);
+}
