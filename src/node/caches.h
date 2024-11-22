dab3a583bb80ad97abb9b8e22f81e8cb0de480e7
TheCharlatan <seb.kung@gmail.com>
2024-11-22 20:51:34 +0100

    kernel: Move kernel-specific cache size options to kernel
    Carrying non-kernel related fields in the cache sizes for the indexes is
    confusing for kernel library users. The cache sizes also are set
    currently with magic numbers in bitcoin-chainstate. The comments for the
    cache size calculations are also not completely clear.
    
    Solve these things by moving the kernel-specific cache size fields to
    their own struct.
    
    This slightly changes the way the cache is allocated if the txindex
    and/or blockfilterindex is used. Since they are now given precedence
    over the block tree db cache, this results in a bit less cache being
    allocated to the block tree db, coinsdb and coins caches. The effect is
    negligible though, i.e. cache sizes with default dbcache reported
    through the logs are:
    
    master:
    Cache configuration:
    * Using 2.0 MiB for block index database
    * Using 56.0 MiB for transaction index database
    * Using 49.0 MiB for basic block filter index database
    * Using 8.0 MiB for chain state database
    * Using 335.0 MiB for in-memory UTXO set (plus up to 286.1 MiB of unused mempool space)
    
    this branch:
    Cache configuration:
    * Using 2.0 MiB for block index database
    * Using 56.2 MiB for transaction index database
    * Using 49.2 MiB for basic block filter index database
    * Using 8.0 MiB for chain state database
    * Using 334.5 MiB for in-memory UTXO set (plus up to 286.1 MiB of unused mempool space)

--- src/node/caches.h
+++ src/node/caches.h
@@ -5,18 +5,21 @@
 #ifndef BITCOIN_NODE_CACHES_H
 #define BITCOIN_NODE_CACHES_H
 
+#include <kernel/caches.h>
+
 #include <cstddef>
 #include <cstdint>
 
 class ArgsManager;
 
 namespace node {
-struct CacheSizes {
-    int64_t block_tree_db;
-    int64_t coins_db;
-    int64_t coins;
+struct IndexCacheSizes {
     int64_t tx_index;
     int64_t filter_index;
+};
+struct CacheSizes {
+    IndexCacheSizes index;
+    kernel::CacheSizes kernel;
 };
 CacheSizes CalculateCacheSizes(const ArgsManager& args, size_t n_indexes = 0);
 } // namespace node
