b5a920447cac6dd938ae8b6ee4da14d6b21c2e4e
TheCharlatan <seb.kung@gmail.com>
2024-11-22 20:51:34 +0100

    kernel: Move kernel-specific cache size options to kernel
    Carrying non-kernel related fields in the cache sizes for the indexes is
    confusing for kernel library users. The cache sizes also are set
    currently with magic numbers in bitcoin-chainstate. The comments for the
    cache size calculations are also not completely clear.
    
    Solve these things by moving the kernel-specific cache size fields to
    their own struct.
    
    This slightly changes the way the cache is allocated if the txindex
    and/or blockfilterindex is used. Since they are now given precedence
    over the block tree db cache, this results in a bit less cache being
    allocated to the block tree db, coinsdb and coins caches. The effect is
    negligible though, i.e. cache sizes with default dbcache reported
    through the logs are:
    
    master:
    Cache configuration:
    * Using 2.0 MiB for block index database
    * Using 56.0 MiB for transaction index database
    * Using 49.0 MiB for basic block filter index database
    * Using 8.0 MiB for chain state database
    * Using 335.0 MiB for in-memory UTXO set (plus up to 286.1 MiB of unused mempool space)
    
    this branch:
    Cache configuration:
    * Using 2.0 MiB for block index database
    * Using 56.2 MiB for transaction index database
    * Using 49.2 MiB for basic block filter index database
    * Using 8.0 MiB for chain state database
    * Using 334.5 MiB for in-memory UTXO set (plus up to 286.1 MiB of unused mempool space)
    
    Co-authored-by: Hodlinator <172445034+hodlinator@users.noreply.github.com>

---
+++ src/kernel/caches.h
@@ -0,0 +1,41 @@
+// Copyright (c) 2024-present The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_KERNEL_CACHES_H
+#define BITCOIN_KERNEL_CACHES_H
+
+#include <txdb.h>
+#include <util/check.h>
+
+#include <algorithm>
+#include <bit>
+#include <cstdint>
+
+//! Guard against truncation of values before converting.
+constexpr size_t MiBToBytes(int64_t mib)
+{
+    Assert(std::countl_zero(static_cast<uint64_t>(mib)) >= 21); // Ensure signed bit is unset + enough zeros to shift.
+    const int64_t bytes{mib << 20};
+    Assert(static_cast<uint64_t>(bytes) <= std::numeric_limits<size_t>::max());
+    return static_cast<size_t>(bytes);
+}
+
+namespace kernel {
+struct CacheSizes {
+    size_t block_tree_db;
+    size_t coins_db;
+    size_t coins;
+
+    CacheSizes(size_t total_cache)
+    {
+        block_tree_db = std::min(total_cache / 8, MiBToBytes(nMaxBlockDBCache));
+        total_cache -= block_tree_db;
+        coins_db = std::min(total_cache / 2, MiBToBytes(nMaxCoinsDBCache));
+        total_cache -= coins_db;
+        coins = total_cache; // the rest goes to the coins cache
+    }
+};
+} // namespace kernel
+
+#endif // BITCOIN_KERNEL_CACHES_H
