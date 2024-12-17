54bdaaefbc683a61f3051dd90bbaf69de5d6cac5
TheCharlatan <seb.kung@gmail.com>
2024-12-17 21:58:47 +0100

    init: Use size_t consistently for cache sizes
    This avoids having to rely on implicit casts when passing them to the
    various functions allocating the caches.
    
    This also fixes a bug where an incorrect amount of memory was allocated
    on 32bit platforms. If the db cache size exceeded the maximum size of a
    32 bit unsigned integer, it would overflow and allocate much less memory
    than instructed to. Fix this by returning an error to the user if the
    passed in dbcache size exceeeds the size of a size_t on that platform.
    
    Add a release not for the change in behaviour on 32-bit systems.
    
    Also take this opportunity to make the total amounts of cache in the
    chainstate manager a size_t too.

--- src/test/util/setup_common.h
+++ src/test/util/setup_common.h
@@ -104,7 +104,7 @@ struct BasicTestingSetup {
  * initialization behaviour.
  */
 struct ChainTestingSetup : public BasicTestingSetup {
-    kernel::CacheSizes m_kernel_cache_sizes{node::CalculateCacheSizes(m_args).kernel};
+    kernel::CacheSizes m_kernel_cache_sizes{node::CalculateCacheSizes(m_args).value().kernel};
     bool m_coins_db_in_memory{true};
     bool m_block_tree_db_in_memory{true};
     std::function<void()> m_make_chainman{};
