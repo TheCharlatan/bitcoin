049caae287068a34892fd5327cf8f0e6f1d79aa4
TheCharlatan <seb.kung@gmail.com>
2025-01-09 11:25:43 +0100

    util: Add MiBToByte conversion header
    It is used in the following commits to increase the safety of
    conversions during cache size calculations.
    
    Co-authored-by: Hodlinator <172445034+hodlinator@users.noreply.github.com>
    Co-authored-by: Ryan Ofsky <ryan@ofsky.org>

---
+++ src/util/byte_conversion.h
@@ -0,0 +1,26 @@
+// Copyright (c) 2025-present The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_UTIL_BYTE_CONVERSION_H
+#define BITCOIN_UTIL_BYTE_CONVERSION_H
+
+#include <tinyformat.h>
+
+#include <cstdint>
+#include <limits>
+#include <stdexcept>
+
+//! Guard against truncation of values before converting.
+constexpr size_t MiBToBytes(int64_t mib)
+{
+    if (mib < 0) {
+        throw std::out_of_range("Value may not be negative.");
+    }
+    if (static_cast<uint64_t>(mib) > std::numeric_limits<size_t>::max() >> 20) {
+        throw std::out_of_range(strprintf("Conversion to bytes of %d does not fit into size_t with maximum value %d.", mib, std::numeric_limits<size_t>::max()));
+    }
+    return mib << 20;
+}
+
+#endif // BITCOIN_UTIL_BYTE_CONVERSION_H
