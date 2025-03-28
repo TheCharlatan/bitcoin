4f518bfd14c5fa960cb764d3f54165f76f8e4d13
TheCharlatan <seb.kung@gmail.com>
2025-03-28 13:42:11 +0100

    Functional test

--- test/functional/tool_bitcoin_chainstate.py
+++ test/functional/tool_bitcoin_chainstate.py
@@ -18,9 +18,9 @@ class BitcoinChainstateTest(BitcoinTestFramework):
         # Set prune to avoid disk space warning.
         self.extra_args = [["-prune=550"]]
 
-    def add_block(self, datadir, input, expected_stderr):
+    def add_block(self, binary_args, input, expected_stderr):
         proc = subprocess.Popen(
-            self.get_binaries().chainstate_argv() + [datadir],
+            binary_args,
             stdin=subprocess.PIPE,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
@@ -35,15 +35,17 @@ class BitcoinChainstateTest(BitcoinTestFramework):
 
     def run_test(self):
         node = self.nodes[0]
-        datadir = node.cli.datadir
+        binary_args = node.binaries.chainstate_argv() + [node.cli.datadir]
+        bitcoind_args = node.binaries.util_argv() + [node.cli.datadir]
         node.stop_node()
 
-        self.log.info(f"Testing bitcoin-chainstate {self.get_binaries().chainstate_argv()} with datadir: {datadir}")
+        self.log.info(f"Testing bitcoin-chainstate: {binary_args}")
+        self.log.info(f"Testing bitcoind: {bitcoind_args}")
         block_one = "010000006fe28c0ab6f1b372c1a6a246ae63f74f931e8365e15a089c68d6190000000000982051fd1e4ba744bbbe680e1fee14677ba1a3c3540bf7b1cdb606e857233e0e61bc6649ffff001d01e362990101000000010000000000000000000000000000000000000000000000000000000000000000ffffffff0704ffff001d0104ffffffff0100f2052a0100000043410496b538e853519c726a2c91e61ec11600ae1390813a627c66fb8be7947be63c52da7589379515d4e0a604f8141781e62294721166bf621e73a82cbf2342c858eeac00000000"
-        self.add_block(datadir, block_one, "Block has not yet been rejected")
-        self.add_block(datadir, block_one, "duplicate")
-        self.add_block(datadir, "00", "Block decode failed")
-        self.add_block(datadir, "", "Empty line found")
+        self.add_block(binary_args, block_one, "Block has not yet been rejected")
+        self.add_block(binary_args, block_one, "duplicate")
+        self.add_block(binary_args, "00", "Block decode failed")
+        self.add_block(binary_args, "", "Empty line found")
 
 if __name__ == "__main__":
     BitcoinChainstateTest(__file__).main()
