7150c60638a4fa00c7f8fb608515343f464ce268
TheCharlatan <seb.kung@gmail.com>
2025-04-02 23:18:44 +0200

    kernel: Add interrupt function to C header
    Calling interrupt can halt long-running functions associated with
    objects that were created through the passed-in context.

--- src/kernel/bitcoinkernel.hpp
+++ src/kernel/bitcoinkernel.hpp
@@ -195,6 +195,8 @@ public:
     /** Check whether this Context object is valid. */
     explicit operator bool() const noexcept { return bool{m_impl}; }
 
+	bool Interrupt() noexcept;
+
     friend class ChainstateManagerOptions;
     friend class ChainstateManager;
 };
