59086801d176b081eb216b4c65408d4cace0145e
TheCharlatan <seb.kung@gmail.com>
2025-04-19 22:55:59 +0200

    validation: Add SpendBlock function
    Move the BIP30 checks from ConnectBlock to a new SpendBlock method.
    This is a move-only change, more content to SpendBlock is added in the
    next commits. The goal is to move logic requiring access to the
    CCoinsViewCache out of ConnectBlock and to the new SpendBlock method.
    SpendBlock will in future handle all UTXO set interactions that
    previously took place in ConnectBlock.
    
    By returning early in case of a BIP30 validation failure, an
    additional failure check for an invalid block reward in case of its
    failure is left out.
    
    Callers of ConnectBlock now also need to call SpendBlock before. This is
    enforced in a future commit by adding a CBlockUndo argument that needs
    to be passed to both.
    
    This is a part of a series of commits for removing access to the
    CCoinsViewCache in consensus verification functions. The goal is to
    allow calling verification functions with pre-fetched, or a user-defined
    set of coins.

--- src/test/coinstatsindex_tests.cpp
+++ src/test/coinstatsindex_tests.cpp
@@ -100,6 +100,8 @@ BOOST_FIXTURE_TEST_CASE(coinstatsindex_unclean_shutdown, TestChain100Setup)
             BOOST_CHECK(CheckBlock(block, state, params.GetConsensus()));
             BOOST_CHECK(m_node.chainman->AcceptBlock(new_block, state, &new_block_index, true, nullptr, nullptr, true));
             CCoinsViewCache view(&chainstate.CoinsTip());
+            const auto block_hash{block.GetHash()};
+            BOOST_CHECK(chainstate.SpendBlock(block, new_block_index, block_hash, view, state));
             BOOST_CHECK(chainstate.ConnectBlock(block, state, new_block_index, view));
         }
         // Send block connected notification, then stop the index without
